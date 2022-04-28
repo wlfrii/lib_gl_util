63b2fc8a2599de7c37fd7afe62c32625b26b7164
wlfrii <longfei.wang@sjtu.edu.cn>
2022-04-28 22:51:53 +0800

    commit gl_window: a GLFWwindow manager

---
+++ gl_window.cpp
@@ -0,0 +1,183 @@
+#include "gl_window.h"
+
+namespace {
+
+/**
+ * @brief GLFW whenever the window size changed (by OS or user resize) this 
+ * callback function executes.
+ * 
+ * @param window 
+ * @param width 
+ * @param height 
+ */
+inline void frameBufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+    /* make sure the viewport matches the new window dimensions; note that
+     width and height will be significantly larger than specified on retina 
+     displays. */
+    glViewport(0, 0, width, height);
+}
+
+}
+
+
+GL_UTIL_BEGIN
+
+
+Window::Window(uint16_t width, uint16_t height, uint8_t ver_major, uint8_t ver_minor)
+    : _win_width(width)
+    , _win_height(height)
+    , _win_name("Displayer")
+{
+    /** Initialize GLFW **/
+    glfwInit();
+    // Configuration
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, ver_major); 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, ver_minor); 
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); 
+#ifdef __APPLE__ 
+    // The following command should be add in Mac OS platform to validate the 
+    // commands above. 
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    /** Create GLFWwindo object **/
+    if(!createGLFWwindow()){
+        exit(-1);
+    }
+
+    /** Initialize GLAD **/
+    // GLAD is used to manage the pointers of OpenGL functions and the right 
+    // header files are included in GLAD, so the intialization for GLAD should 
+    // be done before call any OpenGL interface.
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        GL_LOG("Failed to initialize GLAD.\n");
+        exit(-1);
+    }
+
+    _color = {0.2, 0.3, 0.3, 1.0};
+}
+
+
+Window::~Window()
+{
+    glfwTerminate();
+}
+
+
+void Window::activate()
+{
+    // Activate current window
+    glfwMakeContextCurrent(_window);
+
+    // Make current window visiable
+    glfwWindowHint(GLFW_VISIBLE, GL_TRUE);
+
+    // Monitoring kewboard event
+    if(_callback_kbe){
+        _callback_kbe(this->_window);
+    }
+    else{
+        processKeyboardEvent();
+    }
+}
+
+
+void Window::refresh()
+{
+    // Clear and reset window color, this step is just a STATUS SETTING
+    glClearColor(_color.R, _color.G, _color.B, _color.A);   
+    
+    // Clear previous color buffer and validate current color buffer
+    glClear(GL_COLOR_BUFFER_BIT); 
+
+    // Swap the double buffer
+    glfwSwapBuffers(_window);
+
+    // Check the mouse/keyboard events
+    glfwPollEvents();
+}
+
+
+bool Window::shouldClose()
+{
+    return glfwWindowShouldClose(_window);
+}
+
+
+void Window::release()
+{
+    // Terminate GLFW library
+    glfwTerminate();
+}
+
+
+void Window::setBackgroundColor(uint8_t R, uint8_t G, uint8_t B, uint8_t A)
+{
+    _color.R = R / 255.f;
+    _color.G = G / 255.f;
+    _color.B = B / 255.f;
+    _color.A = A / 255.f;
+}
+
+
+bool Window::setToFullScreen(uint8_t monitor_id)
+{
+    bool ret = false;
+    if(_window)
+    {
+        // Read the number of the available monitor
+        int monitor_count = 0;
+        GLFWmonitor** monitor = glfwGetMonitors(&monitor_count);
+        printf("[%d] monitors have been found.\n", monitor_count);
+        if(monitor_id > monitor_count){
+            printf("The specified monitor index out of valid range.\n");
+            return ret;
+        }
+
+        // Set monitor to display window
+        const GLFWvidmode* mode = glfwGetVideoMode(monitor[monitor_id]);
+        glfwSetWindowMonitor(_window, monitor[monitor_id], 0, 0,
+                mode->width, mode->height, mode->refreshRate);
+    
+    }
+    return ret;
+}
+
+
+// --- PRIVATE ---
+bool Window::createGLFWwindow()
+{
+    _window = glfwCreateWindow(_win_width, _win_height, _win_name.c_str(), nullptr, nullptr);
+    if(_window == nullptr){
+        GL_LOG("Failed to creat GLFW window.\n");
+        glfwTerminate();
+        return false;
+    }
+
+    // Activate current window
+    glfwMakeContextCurrent(_window);
+    
+    // Set the framebuffer resize callback for this window, i.e. resize the 
+    // buffer to viewport.
+    glfwSetFramebufferSizeCallback(_window, ::frameBufferSizeCallback);
+    
+    // Hidden current window since there is nothing to show now.
+    glfwWindowHint(GLFW_VISIBLE, GL_FALSE);
+
+    return true;
+}
+
+void Window::processKeyboardEvent()
+{
+    if(glfwGetKey(_window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(_window, true);
+}
+
+
+void Window::setKeyboardEventCallBack(CallbackKeyboardEvent callbackfunc)
+{
+    _callback_kbe = callbackfunc;
+}
+
+GL_UTIL_END
---
+++ gl_window.h
@@ -0,0 +1,157 @@
+/**--------------------------------------------------------------------
+ *																		
+ *   				        OpenGL Utilities 					
+ *																		
+ * Description:													
+ * This file is part of lib_math. You can redistribute it and or modify 
+ * it to construct your own project. It is wellcome to use this library
+ * in your scientific research work.
+ * 
+ * @file 		gl_window.h 
+ * 
+ * @brief 		A manager for the GLFWwindow object.
+ * 
+ * @author		Longfei Wang
+ * 
+ * @version		2.0.0
+ * 
+ * @date		2021/10/08
+ * 
+ * @license		
+ * 
+ * Copyright (C) 2021 Longfei Wang.
+ * 
+ * --------------------------------------------------------------------
+ * Change History:                        
+ * 
+ * 2022.4.28 Refactor the codes:
+ *   # Complete set to fullscreen interface;
+ *   # Add set user defined keyboard event function interface;
+ *   # Add interface for set background color;
+ * 2021.10.9 Complete the doxygen comments.
+ * -------------------------------------------------------------------*/
+#ifndef GL_UTIL_WINDOW_H_LF
+#define GL_UTIL_WINDOW_H_LF
+/** NOTE, GLAD header file should be included before the GLFW header file, since
+ * the correct OpenGL header files are included in GLAD */
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <cstdint>
+#include <string>
+#include <functional>
+#include "gl_module_ns.h"
+
+struct GLFWwindow;
+
+GL_UTIL_BEGIN
+
+typedef void (*CallbackKeyboardEvent)(GLFWwindow* window);
+
+/**
+ * @brief A windows class which help to manage GLFWwindow object.
+ * The default GLFW version is 3 for MAJOR VERSION and 3 for MINOR VERSION.
+ */
+class Window
+{
+public:
+    /**
+     * @brief Construct a new Window object
+     * 
+     * @param width  The width of the window
+     * @param height  The height of the window
+     * @param ver_major The major version of glfw
+     * @param ver_minor The minor version of glfw
+     * @return  Window object
+     */
+    Window(uint16_t width, uint16_t height, uint8_t ver_major = 3, uint8_t ver_minor = 3);
+    ~Window();
+
+    
+    /**
+     * @brief Activate current window.
+     * This is useful when there are more than one GL Window exist.
+     */
+    void activate();
+
+
+    /**
+     * @brief Swap buffers
+     * Note, OpenGL adopts double-buffers to store the rendered image to avoid
+     * the display flickering that may occur in single buffer mode.
+     */
+    void refresh();
+
+
+    /**
+     * @brief Check the window close flag of current window
+     * @return  
+     *  - true, if the window should be closed
+     *  - false. otherwise
+     */
+    bool shouldClose();
+
+
+    /**
+     * @brief Terminate GLFW library
+     */
+    void release();
+
+
+    /**
+     * @brief Set the background color of the window
+     * 
+     * @param R  The red value
+     * @param G  The blue value
+     * @param B  The green value
+     * @param A  The alpha value
+     */
+    void setBackgroundColor(uint8_t R, uint8_t G, uint8_t B, uint8_t A = 255);
+
+
+    /**
+     * @brief Set current GLFWwindow to full screen object
+     * 
+     * @param monitor_id  The index of the monitor to display (when there are 
+     * more than one monitor exist). O denotes the default monitor.
+     * @return 
+     *  - true, if set successfully
+     *  - false, otherwise
+     */
+    bool setToFullScreen(uint8_t monitor_id = 0);
+
+    
+    /**
+     * @brief Set the keyboard event call back
+     * 
+     * NOTE, the default keyboard event only consider the window close event 
+     * that triggered by key 'Esc' event.
+     * This function can facilate the user to define there keyboard event 
+     * function rather than use the default function.
+     */
+    void setKeyboardEventCallBack(CallbackKeyboardEvent callbackfunc);
+
+private:
+    /* Create GLFW window */ 
+    bool createGLFWwindow();
+    /* Process keyboar event */
+    void processKeyboardEvent();
+
+private:
+    GLFWwindow* _window;        //!< The object of GLFWwindow
+    uint16_t    _win_width;     //!< The width of the window
+    uint16_t    _win_height;    //!< The height of the window
+    std::string _win_name;      //!< The name of the window
+
+    struct{
+        float R;
+        float G;
+        float B;
+        float A;
+    }_color;                    //!< The background color of the window
+
+    // The callback function for keyboard event
+    std::function<void(GLFWwindow* window)> _callback_kbe;
+};
+
+GL_UTIL_END
+#endif // GL_UTIL_WINDOW_H_LF
