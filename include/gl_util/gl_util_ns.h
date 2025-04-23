b4314c489bac24ddd678b86b201602ec323fd521
wlfrii <longfei.wang@sjtu.edu.cn>
2025-04-23 17:53:22 +0800

    add init, check init status function

--- include/gl_util/gl_util_ns.h
+++ include/gl_util/gl_util_ns.h
@@ -31,5 +31,46 @@
 #define GL_UTIL_LOG(fmt, ...) \
     printf("[%s][%s][%d] " fmt, __FILE__, __func__, __LINE__, ##__VA_ARGS__)
 
+#define GL_UTIL_PRINT printf
 
+GL_UTIL_BEGIN
+/**
+ * @brief Initialization of gl_util.
+ * 
+ * @param ver_major The major version of OpenGL context that created by GLFW..
+ * @param ver_minor The minor version of OpenGL context that created by GLFW..
+ * 
+ * @return bool
+ *   @retval 2 The gl_util is just be initialized successfully. 
+ *   @retval 1 The gl_util has been initialized.
+ *   @retval 0 Failed to init gl_util, the app will auto-exit. 
+ */
+uint8_t init(uint8_t ver_major = 4, uint8_t ver_minor = 5);
+
+/**
+ * @brief Check gl_util initialization status.
+ * 
+ * @note Using gl_util without properly initialization may cause core dump error. This 
+ * function is used for avoiding that problems, by printing prompt and safe exit.
+ */
+void checkInitStatus();
+
+/**
+ * @brief Clear the window color and buffer.
+ * Generally, there is no need to call this function, the renderring buffer within a
+ * window will be clear after calling gl_util::Window::clear.
+ * 
+ * @note This function should be called before any glDraw operation. If the
+ * function is called after glDraw operation, the buffer will be cleared.
+ * 
+ * @param R The red value.
+ * @param G The green value.
+ * @param B The blue value.
+ * @param A The alpha value.
+ * @param is_depth_on The flag for clearing depth buffer.
+ */
+void clear(uint8_t R = 0, uint8_t G = 0, uint8_t B = 0, uint8_t A = 255, 
+           bool is_depth_on = false);
+
+GL_UTIL_END
 #endif // GL_WARPPER_NS_H_LF
